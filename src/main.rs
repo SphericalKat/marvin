697183a8900e94aebd4e905939ef4387a8202df2
SphericalKat <amolele@gmail.com>
2021-04-14 06:20:15 +0530

    feat: set up an initial structure for update handling
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use teloxide::{prelude::*, utils::command::BotCommand};
+#[derive(BotCommand)]
+#[command(rename = "lowercase", description = "List of supported commands:")]
+enum Command {
+    #[command(description = "display this text.")]
+    Help,
+}
+
+type Cx = UpdateWithCx<AutoSend<Bot>, Message>;
+
+#[tokio::main]
+async fn main() {
+    run().await;
+}
+
+async fn handler(cx: Cx) -> anyhow::Result<()> {
+    log::debug!("Received an update!");
+
+    // check if update contains any text
+    let text = cx.update.text();
+    if text.is_none() {
+        return Ok(());
+    }
+
+    let cmd = Command::parse(text.unwrap(), "marvin");
+
+    match cmd {
+        Ok(c) => match c {
+            Command::Help => cx
+                .answer(Command::descriptions())
+                .send()
+                .await
+                .map(|_| ())?,
+        }
+        Err(_) => {
+            // non-command update, perform data collection here
+        }
+    }
+
+    Ok(())
+}
+
+async fn run() {
+    // load env config
+    dotenv().ok();
+
+    teloxide::enable_logging!();
+    log::info!("Starting marvin...");
+
+    let bot = Bot::from_env().auto_send();
+    teloxide::repl(bot, handler).await;
 }
