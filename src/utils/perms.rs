05289b71de142d383a2d9c9b4d87202aef0d86c3
SphericalKat <amolele@gmail.com>
2022-02-16 03:05:27 +0530

    refactor(perms): migrate to new dispatcher
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- src/utils/perms.rs
+++ src/utils/perms.rs
@@ -1,21 +1,17 @@
 use crate::BOT_ID;
 use anyhow::anyhow;
 use teloxide::{
-    prelude::Requester,
+    payloads::SendMessageSetters,
+    prelude2::*,
     types::{ChatMember, ChatMemberKind, ChatMemberStatus},
 };
 
-use crate::Cx;
-
-async fn is_bot_admin(cx: &Cx) -> anyhow::Result<()> {
-    if cx.update.chat.is_private() {
+async fn is_bot_admin(bot: &AutoSend<Bot>, message: &Message) -> anyhow::Result<()> {
+    if message.chat.is_private() {
         return Ok(());
     }
 
-    let bot_member: ChatMember = cx
-        .requester
-        .get_chat_member(cx.update.chat_id(), *BOT_ID)
-        .await?;
+    let bot_member: ChatMember = bot.get_chat_member(message.chat.id, *BOT_ID).await?;
 
     if let ChatMemberStatus::Administrator = bot_member.status() {
         return Ok(());
@@ -24,26 +20,31 @@ async fn is_bot_admin(cx: &Cx) -> anyhow::Result<()> {
     Err(anyhow!("Bot is not admin"))
 }
 
-pub async fn require_bot_admin(cx: &Cx) -> anyhow::Result<()> {
-    match is_bot_admin(cx).await {
+pub async fn require_bot_admin(bot: &AutoSend<Bot>, message: &Message) -> anyhow::Result<()> {
+    match is_bot_admin(bot, message).await {
         Ok(_) => Ok(()),
         Err(_) => {
-            cx.reply_to("The bot must be an admin for this to work!")
-                .await?;
+            bot.send_message(
+                message.chat.id,
+                "The bot must be an admin for this to work!",
+            )
+            .reply_to_message_id(message.id)
+            .await?;
             Err(anyhow!("Bot is not admin"))
         }
     }
 }
 
-pub async fn is_user_admin(cx: &Cx, user_id: i64) -> anyhow::Result<()> {
-    if cx.update.chat.is_private() {
+pub async fn is_user_admin(
+    bot: &AutoSend<Bot>,
+    message: &Message,
+    user_id: i64,
+) -> anyhow::Result<()> {
+    if message.chat.is_private() {
         return Ok(());
     }
 
-    let chat_member: ChatMember = cx
-        .requester
-        .get_chat_member(cx.update.chat_id(), user_id)
-        .await?;
+    let chat_member: ChatMember = bot.get_chat_member(message.chat.id, user_id).await?;
 
     match chat_member.status() {
         ChatMemberStatus::Administrator => Ok(()),
@@ -52,33 +53,35 @@ pub async fn is_user_admin(cx: &Cx, user_id: i64) -> anyhow::Result<()> {
     }
 }
 
-pub async fn require_user_admin(cx: &Cx) -> anyhow::Result<()> {
-    let user_id = match cx.update.from() {
+pub async fn require_user_admin(bot: &AutoSend<Bot>, message: &Message) -> anyhow::Result<()> {
+    let user_id = match message.from() {
         Some(user) => user.id,
         None => {
             return Err(anyhow!("User not found"));
         }
     };
 
-    match is_user_admin(cx, user_id).await {
+    match is_user_admin(bot, message, user_id).await {
         Ok(_) => Ok(()),
         Err(_) => {
-            cx.reply_to("You need to be an admin for this to work!")
+            bot.send_message(message.chat.id, "You need to be an admin for this to work!")
+                .reply_to_message_id(message.id)
                 .await?;
             Err(anyhow!("User is not admin"))
         }
     }
 }
 
-pub async fn is_user_restricted(cx: &Cx, user_id: i64) -> anyhow::Result<bool> {
-    if cx.update.chat.is_private() {
+pub async fn is_user_restricted(
+    bot: &AutoSend<Bot>,
+    message: &Message,
+    user_id: i64,
+) -> anyhow::Result<bool> {
+    if message.chat.is_private() {
         return Ok(false);
     }
 
-    let chat_member = cx
-        .requester
-        .get_chat_member(cx.update.chat_id(), user_id)
-        .await?;
+    let chat_member = bot.get_chat_member(message.chat.id, user_id).await?;
 
     let is_restricted = chat_member.kind.can_send_messages()
         && chat_member.kind.can_send_media_messages()
@@ -87,11 +90,11 @@ pub async fn is_user_restricted(cx: &Cx, user_id: i64) -> anyhow::Result<bool> {
     Ok(is_restricted)
 }
 
-pub async fn require_bot_restrict_chat_members(cx: &Cx) -> anyhow::Result<()> {
-    let chat_member = &cx
-        .requester
-        .get_chat_member(cx.update.chat_id(), *BOT_ID)
-        .await?;
+pub async fn require_bot_restrict_chat_members(
+    bot: &AutoSend<Bot>,
+    message: &Message,
+) -> anyhow::Result<()> {
+    let chat_member = bot.get_chat_member(message.chat.id, *BOT_ID).await?;
 
     if let ChatMemberKind::Administrator(adm) = &chat_member.kind {
         if adm.can_restrict_members {
@@ -99,19 +102,23 @@ pub async fn require_bot_restrict_chat_members(cx: &Cx) -> anyhow::Result<()> {
         }
     }
 
-    cx.reply_to("I am missing the required permission for this action: CAN_RESTRICT_MEMBERS.")
-        .await?;
+    bot.send_message(
+        message.chat.id,
+        "I am missing the required permission for this action: CAN_RESTRICT_MEMBERS.",
+    )
+    .reply_to_message_id(message.id)
+    .await?;
     Err(anyhow!("Bot cannot restrict chat members"))
 }
 
-pub async fn require_restrict_chat_members(cx: &Cx) -> anyhow::Result<()> {
-    let user = &cx.update.from();
+pub async fn require_restrict_chat_members(
+    bot: &AutoSend<Bot>,
+    message: &Message,
+) -> anyhow::Result<()> {
+    let user = message.from();
 
     if let Some(user) = user {
-        let chat_member: ChatMember = cx
-            .requester
-            .get_chat_member(cx.update.chat_id(), user.id)
-            .await?;
+        let chat_member: ChatMember = bot.get_chat_member(message.chat.id, user.id).await?;
 
         match &chat_member.kind {
             ChatMemberKind::Owner(_) => {
@@ -126,27 +133,32 @@ pub async fn require_restrict_chat_members(cx: &Cx) -> anyhow::Result<()> {
         }
     }
 
-    cx.reply_to("You're missing the required permission for this action: CAN_RESTRICT_MEMBERS.")
-        .await?;
+    bot.send_message(
+        message.chat.id,
+        "You're missing the required permission for this action: CAN_RESTRICT_MEMBERS.",
+    )
+    .reply_to_message_id(message.id)
+    .await?;
     Err(anyhow!("User cannot restrict chat members"))
 }
 
-pub async fn require_group(cx: &Cx) -> anyhow::Result<()> {
-    let chat = &cx.update.chat;
+pub async fn require_group(bot: &AutoSend<Bot>, message: &Message) -> anyhow::Result<()> {
+    let chat = &message.chat;
     if chat.is_group() || chat.is_supergroup() {
         return Ok(());
     }
 
-    cx.reply_to("This command is meant to be used in a group!")
+    bot.send_message(chat.id, "This command is meant to be used in a group!")
+        .reply_to_message_id(message.id)
         .await?;
     Err(anyhow!("This command is meant to be used in a group"))
 }
 
-pub async fn require_bot_promote_chat_members(cx: &Cx) -> anyhow::Result<()> {
-    let chat_member = &cx
-        .requester
-        .get_chat_member(cx.update.chat_id(), *BOT_ID)
-        .await?;
+pub async fn require_bot_promote_chat_members(
+    bot: &AutoSend<Bot>,
+    message: &Message,
+) -> anyhow::Result<()> {
+    let chat_member = bot.get_chat_member(message.chat.id, *BOT_ID).await?;
 
     if let ChatMemberKind::Administrator(adm) = &chat_member.kind {
         if adm.can_promote_members {
@@ -154,19 +166,23 @@ pub async fn require_bot_promote_chat_members(cx: &Cx) -> anyhow::Result<()> {
         }
     }
 
-    cx.reply_to("I am missing the required permission for this action: CAN_PROMOTE_MEMBERS.")
-        .await?;
+    bot.send_message(
+        message.chat.id,
+        "I am missing the required permission for this action: CAN_PROMOTE_MEMBERS.",
+    )
+    .reply_to_message_id(message.id)
+    .await?;
     Err(anyhow!("Bot cannot promote chat members"))
 }
 
-pub async fn require_promote_chat_members(cx: &Cx) -> anyhow::Result<()> {
-    let user = &cx.update.from();
+pub async fn require_promote_chat_members(
+    bot: &AutoSend<Bot>,
+    message: &Message,
+) -> anyhow::Result<()> {
+    let user = message.from();
 
     if let Some(user) = user {
-        let chat_member: ChatMember = cx
-            .requester
-            .get_chat_member(cx.update.chat_id(), user.id)
-            .await?;
+        let chat_member: ChatMember = bot.get_chat_member(message.chat.id, user.id).await?;
 
         match &chat_member.kind {
             ChatMemberKind::Owner(_) => {
@@ -181,19 +197,23 @@ pub async fn require_promote_chat_members(cx: &Cx) -> anyhow::Result<()> {
         }
     }
 
-    cx.reply_to("You're missing the required permission for this action: CAN_PROMOTE_MEMBERS.")
-        .await?;
+    bot.send_message(
+        message.chat.id,
+        "You're missing the required permission for this action: CAN_PROMOTE_MEMBERS.",
+    )
+    .reply_to_message_id(message.id)
+    .await?;
     Err(anyhow!("User cannot promote chat members"))
 }
 
-pub async fn require_can_pin_messages(cx: &Cx) -> anyhow::Result<()> {
-    let user = &cx.update.from();
+pub async fn require_can_pin_messages(
+    bot: &AutoSend<Bot>,
+    message: &Message,
+) -> anyhow::Result<()> {
+    let user = message.from();
 
     if let Some(user) = user {
-        let chat_member: ChatMember = cx
-            .requester
-            .get_chat_member(cx.update.chat_id(), user.id)
-            .await?;
+        let chat_member: ChatMember = bot.get_chat_member(message.chat.id, user.id).await?;
 
         match &chat_member.kind {
             ChatMemberKind::Owner(_) => {
@@ -208,16 +228,20 @@ pub async fn require_can_pin_messages(cx: &Cx) -> anyhow::Result<()> {
         }
     }
 
-    cx.reply_to("You're missing the required permission for this action: CAN_PIN_MESSAGES.")
-        .await?;
+    bot.send_message(
+        message.chat.id,
+        "You're missing the required permission for this action: CAN_PIN_MESSAGES.",
+    )
+    .reply_to_message_id(message.id)
+    .await?;
     Err(anyhow!("User cannot pin messages"))
 }
 
-pub async fn require_bot_can_pin_messages(cx: &Cx) -> anyhow::Result<()> {
-    let chat_member = &cx
-        .requester
-        .get_chat_member(cx.update.chat_id(), *BOT_ID)
-        .await?;
+pub async fn require_bot_can_pin_messages(
+    bot: &AutoSend<Bot>,
+    message: &Message,
+) -> anyhow::Result<()> {
+    let chat_member = bot.get_chat_member(message.chat.id, *BOT_ID).await?;
 
     if let ChatMemberKind::Administrator(adm) = &chat_member.kind {
         if adm.can_pin_messages.unwrap_or(false) {
@@ -225,7 +249,11 @@ pub async fn require_bot_can_pin_messages(cx: &Cx) -> anyhow::Result<()> {
         }
     }
 
-    cx.reply_to("I am missing the required permission for this action: CAN_PIN_MESSAGES.")
-        .await?;
+    bot.send_message(
+        message.chat.id,
+        "I am missing the required permission for this action: CAN_PIN_MESSAGES.",
+    )
+    .reply_to_message_id(message.id)
+    .await?;
     Err(anyhow!("Bot cannot pin messages"))
 }
