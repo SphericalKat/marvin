d88934ff1e34a4d65d5a620f33c7c452bfbab743
SphericalKat <amolele@gmail.com>
2022-02-16 03:27:49 +0530

    refactor(muting): migrate to new dispatcher
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- src/handlers/muting.rs
+++ src/handlers/muting.rs
@@ -3,52 +3,59 @@ use std::convert::TryInto;
 use chrono::Duration;
 use sqlx::{Pool, Postgres};
 use teloxide::{
-    prelude::*,
+    payloads::SendMessageSetters,
+    prelude2::*,
     types::{ChatMember, ChatMemberStatus, ChatPermissions},
 };
 
 use anyhow::anyhow;
 
+use crate::utils::{self, perms};
 use crate::{utils::UnitOfTime, BOT_ID};
-use crate::{
-    utils::{self, perms},
-    Cx,
-};
 
-pub async fn mute(cx: Cx, is_tmute: bool, pool: &Pool<Postgres>) -> anyhow::Result<()> {
-    let chat = &cx.update.chat;
+pub async fn mute(
+    bot: &AutoSend<Bot>,
+    message: &Message,
+    is_tmute: bool,
+    pool: &Pool<Postgres>,
+) -> anyhow::Result<()> {
+    let chat = &message.chat;
 
     // check for required conditions
     tokio::try_join!(
-        perms::require_group(&cx), // command needs to be in a public group
-        perms::require_restrict_chat_members(&cx), // user requires RESTRICT_CHAT_MEMBERS permissions
-        perms::require_bot_restrict_chat_members(&cx) // bot requires RESTRICT_CHAT_MEMBERS permissions
+        perms::require_group(bot, message), // command needs to be in a public group
+        perms::require_restrict_chat_members(bot, message), // user requires RESTRICT_CHAT_MEMBERS permissions
+        perms::require_bot_restrict_chat_members(bot, message) // bot requires RESTRICT_CHAT_MEMBERS permissions
     )?;
 
     // extract user and text from message
-    let (user_id, args) = utils::extract_user_and_text(&cx, pool).await;
+    let (user_id, args) = utils::extract_user_and_text(bot, message, pool).await;
     if user_id.is_none() {
         // no user was targeted
-        cx.reply_to("Try targeting a user next time bud.").await?;
+        bot.send_message(message.chat.id, "Try targeting a user next time bud.")
+            .reply_to_message_id(message.id)
+            .await?;
         return Ok(());
     }
 
     // user didn't specify a time for temp mute
     if args.is_none() && is_tmute {
-        cx.reply_to("You need to specify a duration in d/h/m/s (days, hours, minutes, seconds)")
-            .await?;
+        bot.send_message(
+            message.chat.id,
+            "You need to specify a duration in d/h/m/s (days, hours, minutes, seconds)",
+        )
+        .reply_to_message_id(message.id)
+        .await?;
         return Ok(());
     }
 
     // check if user is valid
-    let chat_member: ChatMember = match cx
-        .requester
-        .get_chat_member(chat.id, user_id.unwrap())
-        .await
-    {
+    let chat_member: ChatMember = match bot.get_chat_member(chat.id, user_id.unwrap()).await {
         Ok(m) => m, // user is valid
         Err(_) => {
-            cx.reply_to("This user is ded mate.").await?; // invalid user (outdated info in db?)
+            bot.send_message(message.chat.id, "This user is ded mate.")
+                .reply_to_message_id(message.id)
+                .await?; // invalid user (outdated info in db?)
             return Ok(());
         }
     };
@@ -56,13 +63,17 @@ pub async fn mute(cx: Cx, is_tmute: bool, pool: &Pool<Postgres>) -> anyhow::Resu
     match chat_member.status() {
         // don't try to mute admins
         ChatMemberStatus::Administrator | ChatMemberStatus::Owner => {
-            cx.reply_to("I'm not muting an administrator!").await?;
+            bot.send_message(message.chat.id, "I'm not muting an administrator!")
+                .reply_to_message_id(message.id)
+                .await?;
             return Ok(());
         }
 
         // don't try to mute users not in the chat
         ChatMemberStatus::Banned | ChatMemberStatus::Left => {
-            cx.reply_to("This user isn't in the chat!").await?;
+            bot.send_message(message.chat.id, "This user isn't in the chat!")
+                .reply_to_message_id(message.id)
+                .await?;
             return Ok(());
         }
         _ => {}
@@ -70,12 +81,12 @@ pub async fn mute(cx: Cx, is_tmute: bool, pool: &Pool<Postgres>) -> anyhow::Resu
 
     // user is a dumbass
     if user_id.unwrap() == *BOT_ID {
-        cx.reply_to("No u").await?;
+        bot.send_message(message.chat.id, "No u").await?;
         return Ok(());
     }
 
     // check if user is already restricted
-    let is_restricted = perms::is_user_restricted(&cx, user_id.unwrap()).await?;
+    let is_restricted = perms::is_user_restricted(bot, message, user_id.unwrap()).await?;
 
     let permissions = ChatPermissions::empty();
 
@@ -84,77 +95,92 @@ pub async fn mute(cx: Cx, is_tmute: bool, pool: &Pool<Postgres>) -> anyhow::Resu
             // get unit of time
             let unit = args.parse::<UnitOfTime>();
             if unit.is_err() {
-                cx.reply_to("failed to get specified time; expected one of d/h/m/s (days, hours, minutes, seconds)").await?;
+                bot.send_message(message.chat.id,"failed to get specified time; expected one of d/h/m/s (days, hours, minutes, seconds)")
+								.reply_to_message_id(message.id)
+								.await?;
                 return Ok(());
             }
 
             // convert to seconds
             let time = utils::extract_time(unit.as_ref().unwrap());
-            let until_time = cx
-                .update
+            let until_time = message
                 .date
                 .checked_add_signed(Duration::seconds(time.try_into().unwrap()))
                 .ok_or(anyhow!("Something went wrong!"))?;
 
             // mute chat member for specified time
-            cx.requester
-                .restrict_chat_member(chat.id, user_id.unwrap(), permissions)
+            bot.restrict_chat_member(chat.id, user_id.unwrap(), permissions)
                 .until_date(until_time)
                 .await?;
 
             if is_restricted {
-                cx.reply_to(format!(
-                    "Restrictions have been updated. Muted for {}!",
-                    unit.unwrap()
-                ))
+                bot.send_message(
+                    message.chat.id,
+                    format!(
+                        "Restrictions have been updated. Muted for {}!",
+                        unit.unwrap()
+                    ),
+                )
+                .reply_to_message_id(message.id)
                 .await?;
             } else {
-                cx.reply_to(format!("Muted for {}!", unit.unwrap())).await?;
+                bot.send_message(message.chat.id, format!("Muted for {}!", unit.unwrap()))
+                    .reply_to_message_id(message.id)
+                    .await?;
             }
         }
     } else {
         // permanently mute chat member
-        cx.requester
-            .restrict_chat_member(chat.id, user_id.unwrap(), permissions)
+        bot.restrict_chat_member(chat.id, user_id.unwrap(), permissions)
             .await?;
 
         if is_restricted {
-            cx.reply_to("Restrictions have been updated. Permanently muted!")
-                .await?;
+            bot.send_message(
+                message.chat.id,
+                "Restrictions have been updated. Permanently muted!",
+            )
+            .reply_to_message_id(message.id)
+            .await?;
         } else {
-            cx.reply_to("Muted!").await?;
+            bot.send_message(message.chat.id, "Muted!")
+                .reply_to_message_id(message.id)
+                .await?;
         }
     }
 
     Ok(())
 }
 
-pub async fn unmute(cx: Cx, pool: &Pool<Postgres>) -> anyhow::Result<()> {
-    let chat = &cx.update.chat;
+pub async fn unmute(
+    bot: &AutoSend<Bot>,
+    message: &Message,
+    pool: &Pool<Postgres>,
+) -> anyhow::Result<()> {
+    let chat = &message.chat;
 
     // check for required conditions
     tokio::try_join!(
-        perms::require_group(&cx), // command needs to be in a public group
-        perms::require_restrict_chat_members(&cx), // user requires RESTRICT_CHAT_MEMBERS permissions
-        perms::require_bot_restrict_chat_members(&cx) // bot requires RESTRICT_CHAT_MEMBERS permissions
+        perms::require_group(bot, message), // command needs to be in a public group
+        perms::require_restrict_chat_members(bot, message), // user requires RESTRICT_CHAT_MEMBERS permissions
+        perms::require_bot_restrict_chat_members(bot, message) // bot requires RESTRICT_CHAT_MEMBERS permissions
     )?;
 
     // extract user and text from message
-    let (user_id, _) = utils::extract_user_and_text(&cx, pool).await;
+    let (user_id, _) = utils::extract_user_and_text(bot, message, pool).await;
     if user_id.is_none() {
-        cx.reply_to("Try targeting a user next time bud.").await?;
+        bot.send_message(message.chat.id, "Try targeting a user next time bud.")
+            .reply_to_message_id(message.id)
+            .await?;
         return Ok(());
     }
 
     // check if user is valid
-    let chat_member: ChatMember = match cx
-        .requester
-        .get_chat_member(chat.id, user_id.unwrap())
-        .await
-    {
+    let chat_member: ChatMember = match bot.get_chat_member(chat.id, user_id.unwrap()).await {
         Ok(m) => m, // user is valid
         Err(_) => {
-            cx.reply_to("This user is ded mate.").await?; // invalid user
+            bot.send_message(message.chat.id, "This user is ded mate.")
+                .reply_to_message_id(message.id)
+                .await?; // invalid user
             return Ok(());
         }
     };
@@ -164,19 +190,24 @@ pub async fn unmute(cx: Cx, pool: &Pool<Postgres>) -> anyhow::Result<()> {
         chat_member.status(),
         ChatMemberStatus::Banned | ChatMemberStatus::Left
     ) {
-        cx.reply_to("This user isn't in the chat!").await?;
+        bot.send_message(message.chat.id, "This user isn't in the chat!")
+            .reply_to_message_id(message.id)
+            .await?;
         return Ok(());
     }
 
     // don't try to unmute unrestricted users
-    if !perms::is_user_restricted(&cx, user_id.unwrap()).await? {
-        cx.reply_to("This user can already speak freely!").await?;
+    if !perms::is_user_restricted(bot, message, user_id.unwrap()).await? {
+        bot.send_message(message.chat.id, "This user can already speak freely!")
+            .await?;
         return Ok(());
     }
 
     // user is a dumbass
     if user_id.unwrap() == *BOT_ID {
-        cx.reply_to("What exactly are you trying to do?").await?;
+        bot.send_message(message.chat.id, "What exactly are you trying to do?")
+            .reply_to_message_id(message.id)
+            .await?;
         return Ok(());
     }
 
@@ -186,19 +217,15 @@ pub async fn unmute(cx: Cx, pool: &Pool<Postgres>) -> anyhow::Result<()> {
         | ChatPermissions::SEND_OTHER_MESSAGES
         | ChatPermissions::SEND_POLLS
         | ChatPermissions::ADD_WEB_PAGE_PREVIEWS;
-    // .can_send_messages(true)
-    // .can_send_media_messages(true)
-    // .can_send_other_messages(true)
-    // .can_send_polls(true)
-    // .can_add_web_page_previews(true);
 
     // unmute the user
-    cx.requester
-        .restrict_chat_member(chat.id, user_id.unwrap(), permissions)
+    bot.restrict_chat_member(chat.id, user_id.unwrap(), permissions)
         .await?;
 
     // let user know something happened
-    cx.reply_to("Unmuted!").await?;
+    bot.send_message(message.chat.id, "Unmuted!")
+        .reply_to_message_id(message.id)
+        .await?;
 
     Ok(())
 }
